046435d6a8b693096dc434841c19180d1796d119
Alex Costa & Steve Smietana <student@epicodus.com>
2015-08-04 15:26:48 -0700

    created contact class

--- src/Contact.php
+++ src/Contact.php
@@ -11,6 +11,25 @@ public class Contact {
       $this->email = $email;
       $this->address = $address;
     }
+
+    function getPhone() {
+      return $this->phone;
+    }
+    function getEmail() {
+      return $this->email;
+    }
+    function getAddress() {
+      return $this->address;
+    }
+    function setPhone($new_phone) {
+      $this->phone = $new_phone;
+    }
+    function setEmail($new_email) {
+      $this->email = $new_email;
+    }
+    function setAddress($new_address) {
+      $this->address = $new_address;
+    }
 }
 
  ?>
